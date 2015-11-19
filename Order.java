1c7db2a750972259ed0fc067b8ab6796db153d4a
SoftwareEngineeringComp2140 <altonbodley@gmail.com>
2015-11-19 12:47:27 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,30 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IGGUI;
 
 /**
- * Write a description of class khh here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ *
+ * @author AltonJr
  */
-public class Order
-{
-   private static int ordNum;
+import java.util.*;
+public class Order {
+    private static int ordNum;
    private float price;
-   //private Menu item; //combo,special,customizedItem;
-  // private Date time;
-   private Payment subtotal, total;
-   private float gct;
-   
-//combo = new Menu;
-   Menu special = new Menu();
-   Menu customizedItem = new Menu();
-   Date time = new Date();  
-   
-   public Order(Menu item)
+   //private Menu item, combo,special,customizedItem;
+   //private Time time;
+   private Payment subtl, total;
+   private float gct,p;
+   private int q;
+   private ArrayList order;
+
+   private Menu combo = new Menu();
+   private Menu special = new Menu();
+   private Menu customizedItem = new Menu();
+   private Menu item = new Menu();
+   private Time time = new Time();
+   private Payment subtotal = new Payment();
+   private Mcombo ord = new Mcombo();
+   /***
+    * Constructor for order class. 
+    * @param: null
+    * @return: null
+    */
+   public Order()
    {
-       this.ordNum = ordNum;
-       this.item = item; 
-       
-    }
+       //this.item = item;
+       ordNum = getOrdNum(); // Associates Order number with Order
+       time = getTime(); //Asociates a time/stopwatch with order
+       order=ord.getOrdList(); // accepts order as a list
+   }
     
    public int getOrdNum()
    {
@@ -41,28 +55,35 @@ public class Order
        return combo;
    }
    
-   public Menu getSpecial()
+   /*public Menu getSpecial()
    {
        return special;
-   }
+   }*/
    
-    public Menu getCustomizedItem()
+    /*public Menu getCustomizedItem()
    {
        return customizedItem;
-   }
+   }*/
    
-   public Date getTime()
+   public Time getTime()
    {
        return time;
    }
    
-   
-   public Payment getTotalPrice()
+   public void setGct(float gct)
    {
-       subtotal= price;
-       return (subtotal+total);
-   
+       this.gct=gct;       
    }
    
+   public float getGct()
+   {
+       return gct;
+   }
    
+  /* public float getTotalPrice()
+   {
+       subtl = (subtotal.totalPrice(float p, int q));
+        //totalPrice;
+       return (subtl * (1+gct));
+}*/
 }
